979f849391cf29c4705c3acf5026a7b860c5228c
Sky Morey <smorey@degdigital.com>
2019-05-04 10:44:48 -0500

    updates

---
+++ src/Loaders/GameNetCore/src/Loader/Program.cs
@@ -0,0 +1,19 @@
+﻿using Contoso.GameNetCore;
+using Contoso.GameNetCore.Hosting;
+using Test = Gamer.Asset.Tes.Loader.LoadObject;
+
+namespace Loader
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Test.Start();
+            //CreateGameHostBuilder(args).Build().Run();
+        }
+
+        public static IGameHostBuilder CreateGameHostBuilder(string[] args) =>
+            GameHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ src/Loaders/GameNetCore/src/Loader/Startup.cs
@@ -0,0 +1,28 @@
+using Contoso.GameNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Loader
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public static IConfiguration Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            ////services.AddDistributedMemoryCache();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+        }
+    }
+}
