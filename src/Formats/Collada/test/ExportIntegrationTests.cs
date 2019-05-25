f28725b31974f08be8737cc416f8092a02537a1e
Sky Morey <smorey@degdigital.com>
2019-05-25 11:13:46 -0500

    updates

--- src/Formats/Collada/test/ExportIntegrationTests.cs
+++ src/Formats/Collada/test/ExportIntegrationTests.cs
@@ -15,9 +15,9 @@ namespace Gamer.Estate.Collada.Tests
 
         [Theory]
         //[InlineData(@"Objects\animals\fish\CleanerFish_clean_prop_animal_01.chr")]
-        //[InlineData(@"Objects\buildingsets\human\hightech\prop\hydroponic\hydroponic_machine_1_incubator_01x01x02_a.cgf")]
+        [InlineData(@"Objects\buildingsets\human\hightech\prop\hydroponic\hydroponic_machine_1_incubator_01x01x02_a.cgf")]
         //[InlineData(@"Objects\buildingsets\human\hightech\prop\hydroponic\hydroponic_machine_1_incubator_02x01x012_a.cgf")]
-        [InlineData(@"Objects\buildingsets\human\hightech\prop\hydroponic\hydroponic_machine_1_incubator_rotary_025x01x0225_a.cga")]
+        //[InlineData(@"Objects\buildingsets\human\hightech\prop\hydroponic\hydroponic_machine_1_incubator_rotary_025x01x0225_a.cga")]
         //[InlineData(@"Objects\buildingsets\human\hightech\prop\hydroponic\hydroponic_machine_1_incubator_rotary_025x01x0225_a.cgf")]
         public void LoadModel(string path)
         {
