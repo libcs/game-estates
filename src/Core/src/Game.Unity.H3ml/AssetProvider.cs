3cb7ad0a0c4674e1502b73b126270a7a7cd22bd4
Sky Morey <smorey@degdigital.com>
2019-08-31 18:04:07 -0500

    updates

---
+++ src/Core/src/Game.Unity.H3ml/AssetProvider.cs
@@ -0,0 +1,19 @@
+﻿using H3ml.Asset;
+using System.Collections.Generic;
+
+namespace Game.Unity
+{
+    public class AssetProvider : IAssetProvider
+    {
+        public object CreateObject(string url, Dictionary<string, string> attributes)
+        {
+            return null;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public string MakeKey(string url, Dictionary<string, string> attributes) => url;
+    }
+}
